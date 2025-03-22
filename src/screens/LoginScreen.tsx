67992ce7bd5240df6d1cb483325147731d1d9aca
Vivek Misra <vimis22@student.sdu.dk>
2025-03-23 00:55:52 +0100

    Recently completed most of the styling of LoginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import GlobalStyles from '../components/GlobalStyles.tsx';
-import InputFieldArea from '../components/InputFieldArea.tsx';
 
 const LoginScreen = ({navigation}: any) => {
     return (
         <SafeAreaView style={GlobalStyles.pageContainer}>
-            <InputFieldArea value={'value'} onChangeText={changeMethodInsertHere} backgroundColor={'#330099'} textColor={'#ffffff'}/>
         </SafeAreaView>
     );
 };
