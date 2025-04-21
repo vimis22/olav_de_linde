b567c82ec5a100e658c9f9d970afb28b6baf3a41
Vivek Misra <vimis22@student.sdu.dk>
2025-04-21 20:29:20 +0200

    Currently working on Tabbar on HomeScreen with Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import CreateCompanySignup from './src/screens/authentication_area/signup/CreateCompanySignup.tsx';
 import SplashScreen from './src/screens/authentication_area/SplashScreen.tsx';
 import LoginScreen from './src/screens/authentication_area/login/LoginScreen.tsx';
 import ForgotPasswordScreen from './src/screens/authentication_area/signup/ForgotPasswordScreen.tsx';
 import auth from '@react-native-firebase/auth';
-import {LogBox} from 'react-native';
+import {LogBox, View, Image, Text, StyleSheet} from 'react-native';
 import CreateNameSignup from './src/screens/authentication_area/signup/CreateNameSignup.tsx';
 import CreatePasswordSignup from './src/screens/authentication_area/signup/CreatePasswordSignup.tsx';
 import SignupVerification from './src/screens/authentication_area/signup/SignupVerification.tsx';
-import BiometricsVerification from "./src/screens/authentication_area/signup/BiometricsVerification.tsx";
-import NotificationVerification from "./src/screens/authentication_area/signup/NotificationVerification.tsx";
+import BiometricsVerification from './src/screens/authentication_area/signup/BiometricsVerification.tsx';
+import NotificationVerification from './src/screens/authentication_area/signup/NotificationVerification.tsx';
 import HomeScreen from './src/screens/authentication_area/home/HomeScreen.tsx';
+import CaseScreen from './src/screens/authentication_area/home/CaseScreen.tsx';
+import SettingsScreen from './src/screens/authentication_area/home/SettingsScreen.tsx';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     useEffect(() => {
@@ -33,22 +37,118 @@ export default function App() {
     LogBox.ignoreLogs(['AsyncStorage has been extracted']);
 
 
-    return(
-        <GestureHandlerRootView>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName={'SplashScreen'}>
-                    <Stack.Screen name={'SplashScreen'} component={SplashScreen}/>
-                    <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
-                    <Stack.Screen name={'ForgotPasswordScreen'} component={ForgotPasswordScreen} />
-                    <Stack.Screen name={'CreateCompanySignup'} component={CreateCompanySignup} />
-                    <Stack.Screen name={'CreateNameSignup'} component={CreateNameSignup} />
-                    <Stack.Screen name={'CreatePasswordSignup'} component={CreatePasswordSignup} />
-                    <Stack.Screen name={'SignupVerification'} component={SignupVerification} />
-                    <Stack.Screen name={'BiometricsVerification'} component={BiometricsVerification} />
-                    <Stack.Screen name={'NotificationVerification'} component={NotificationVerification} />
-                    <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </GestureHandlerRootView>
+    return (
+      <GestureHandlerRootView style={{flex: 1}}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName={'SplashScreen'}
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen name={'SplashScreen'} component={SplashScreen} />
+            <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
+            <Stack.Screen
+              name={'ForgotPasswordScreen'}
+              component={ForgotPasswordScreen}
+            />
+            <Stack.Screen
+              name={'CreateCompanySignup'}
+              component={CreateCompanySignup}
+            />
+            <Stack.Screen
+              name={'CreateNameSignup'}
+              component={CreateNameSignup}
+            />
+            <Stack.Screen
+              name={'CreatePasswordSignup'}
+              component={CreatePasswordSignup}
+            />
+            <Stack.Screen
+              name={'SignupVerification'}
+              component={SignupVerification}
+            />
+            <Stack.Screen
+              name={'BiometricsVerification'}
+              component={BiometricsVerification}
+            />
+            <Stack.Screen
+              name={'NotificationVerification'}
+              component={NotificationVerification}
+            />
+            <Stack.Screen name={'HomeScreen'} component={NavbarNavigation} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
     );
-}
+};
+
+function NavbarNavigation(){
+  return(
+    <Tab.Navigator screenOptions={({route}) => ({
+      tabBarShowLabel: false,
+      tabBarIcon: ({focused}) => {
+        let source;
+        let title;
+
+        switch (route.name) {
+          case 'HomeScreen':
+            source = require('./src/assets/OLD_HomeIcon.png');
+            title = 'HJEM';
+            break;
+          case 'CaseScreen':
+            source = require('./src/assets/OLD_PlusIcon.png');
+            title = 'OPRET';
+            break;
+          case 'SettingsScreen':
+            source = require('./src/assets/OLD_MoreIcon.png');
+            title = 'MERE';
+            break;
+        }
+        return (
+          <View>
+            <Image source={source} style={[styles.icon, focused ? styles.iconFocused : styles.iconUnfocused]} resizeMode={'contain'} />
+            <Text style={[styles.label, focused && styles.labelFocused]}>{title}</Text>
+          </View>
+        )
+      }
+    })}>
+      <Tab.Screen name={'HomeScreen'} component={HomeScreen} />
+      <Tab.Screen name={'CaseScreen'} component={CaseScreen} />
+      <Tab.Screen name={'SettingsScreen'} component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    height: 75,
+    backgroundColor: 'white',
+    borderTopColor: '#eee',
+    borderTopWidth: 1,
+    paddingBottom: 8,
+    paddingTop: 6,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  icon: {
+    width: 26,
+    height: 26,
+    marginBottom: 4,
+  },
+  iconFocused: {
+    tintColor: '#ff6a00',
+  },
+  iconUnfocused: {
+    tintColor: '#999',
+  },
+  label: {
+    fontSize: 8,
+    textAlign: 'center',
+    color: 'black'
+  },
+  labelFocused: {
+    color: '#ff6a00',
+    fontWeight: 6,
+  },
+});
---
+++ src/screens/authentication_area/home/SettingsScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ImageBackground, View} from 'react-native';
+import GlobalStyles, {wallpaperBackground} from '../../../Styling/GlobalStyles.tsx';
+import NormalText from '../../../components/NormalText.tsx';
+
+const SettingsScreen = () => {
+  return (
+    <ImageBackground source={wallpaperBackground} style={GlobalStyles.backgroundImage} resizeMode={'cover'}>
+      <View>
+        <NormalText text={'CaseScreen'} fontSize={10} fontWeight={'bold'}/>
+      </View>
+    </ImageBackground>
+  )
+};
+
+export default SettingsScreen;
